c7d40501ef63f26b85f6c697652556bc9f93ca5e
Josue Amorim <josuedesouzaamorim@gmail.com>
2022-08-04 11:00:44 -0300

    I Implemented locationManager func to get the current location. The weather infos are  now printed on screen.

--- Weather/Controller/WeatherViewController.swift
+++ Weather/Controller/WeatherViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
     
@@ -16,13 +17,28 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var searchTextField: UITextField!
     
     var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        temperatureLabel.text = "--"
+        cityLabel.text = "Loading"
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestLocation()
+        
         searchTextField.delegate = self
         weatherManager.delegate =  self
         
     }
+    
+    @IBAction func myLocation(_ sender: UIButton) {
+        //get my location 
+    }
+    
 }
 
 // MARK: - UITextFieldDelegate
@@ -61,11 +77,32 @@ extension WeatherViewController: UITextFieldDelegate {
 extension WeatherViewController: WeatherManagerDelegate {
     
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel){
-        print(weather.temperatureString)
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.cityLabel.text = weather.cityName
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
     }
     
     func didFailWithError(error: Error) {
         print(error)
+    }
+}
+
+// MARK: - didUpdateLocationsDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get users location.")
     }
 }
 
